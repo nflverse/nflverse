5ae92dc0a7842ee596d16a7a1411fc84ac33c1ea
mrcaseb <roemerle@gmail.com>
2021-04-16 17:27:47 +0200

    add package documentation page

--- R/nflverse-package.R
+++ R/nflverse-package.R
@@ -1,4 +1,4 @@
-#' @keywords internal
+#' The 'nflverse' - A set of packages dedicated to data of the National Football League.
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
