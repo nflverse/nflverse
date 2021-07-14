b74726a6f35b81e0c6f997bb1f11a71c883a3f9c
mrcaseb <roemerle@gmail.com>
2021-07-14 14:25:54 +0200

    add tests and fix reports

--- R/reports.R
+++ R/reports.R
@@ -9,13 +9,10 @@ nflverse_packages <- function(include_self = TRUE) {
   imports <- strsplit(raw, ",")[[1]]
   parsed <- gsub("^\\s+|\\s+$", "", imports)
   names <- vapply(strsplit(parsed, "\\s+"), "[[", 1, FUN.VALUE = character(1))
-
   if (include_self) {
     names <- c(names, "nflverse")
   }
-
-  tool_pkgs <- c("cli", "crayon", "rstudioapi", "magrittr")
-  names <- names[!names %in% tool_pkgs] %>% sort()
-
+  exclude <- c("cli", "crayon", "rstudioapi", "magrittr")
+  names <- names[!names %in% exclude] %>% sort()
   names
 }
--- R/utils.R
+++ R/utils.R
@@ -24,19 +24,6 @@ text_col <- function(x) {
 
 }
 
-nflverse_packages <- function(include_self = TRUE) {
-  raw <- utils::packageDescription("nflverse")$Imports
-  imports <- strsplit(raw, ",")[[1]]
-  parsed <- gsub("^\\s+|\\s+$", "", imports)
-  names <- vapply(strsplit(parsed, "\\s+"), "[[", 1, FUN.VALUE = character(1))
-
-  if (include_self) {
-    names <- c(names, "nflverse")
-  }
-
-  names
-}
-
 invert <- function(x) {
   if (length(x) == 0) return()
   stacked <- utils::stack(x)
